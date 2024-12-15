dc77a75ab5273e60d99b1d4aac81f301fd3b022a
LaerkeI <imeland3@gmail.com>
2024-12-15 16:47:33 +0100

    Ebook.cs changed to EbookDto and moved to /Shared/DTOs

--- OrderManagementService/Shared/DTOs/EbookDto.cs
+++ OrderManagementService/Shared/DTOs/EbookDto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shared.Models
+namespace Shared.DTOs
 {
-    public class Ebook
+    public class EbookDto
     {
         public string ISBN { get; set; }
     }
